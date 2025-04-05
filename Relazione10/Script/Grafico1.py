edea0473e25f6d20ce7068afe42f4b640ff0c090
Gerardo Selce <gerardoselce28@gmail.com>
2025-04-05 15:55:34 +0200

    relazione10

--- Relazione10/Script/Grafico1.py
+++ Relazione10/Script/Grafico1.py
@@ -7,7 +7,16 @@ data = np.loadtxt('dati')
 OD = 19.5 / 100
 OpD = data[:, 0] / 100
 h = data[:, 1] / 100
-theta_i = 62.06
+d = 0.575
+D0 = 0.4235
+D = 0.1185
+delta_d = 0.0005
+delta_D = 0.0005
+delta_D0 = 0.0005
+
+theta_i = math.degrees(np.arctan(d/(D0-D)))
+delta_theta_i = math.degrees(((D0-D)/(d**2+D0**2-2*D*D0+D**2))*delta_d - ((d)/(D0**2-2*D*D0+D**2+d**2))*delta_D0 + ((d)/(D**2-2*D0*D+D0**2+d**2))*delta_D)
+print("theta_i = ",theta_i," +- ", delta_theta_i)
 
 y = OD-OpD
 x = h
@@ -48,13 +57,21 @@ sigma_b = sigmay*np.sqrt(len(x)/delta)
 delta_a = 3*sigma_a
 delta_b = 3*sigma_b
 
-theta_r = np.arctan(np.tan(math.radians(theta_i)) - b)
+tan_theta_i = np.tan(math.radians(theta_i))
+delta_tan_theta_i = (1/(np.cos(math.radians(theta_i))**2))*delta_theta_i
 
-print("theta_r: ", theta_r)
+theta_r = np.arctan(np.tan(math.radians(theta_i)) - b)
+tir = math.radians(theta_i)
+delta_theta_r = np.abs((1/(np.cos(tir)**2*np.tan(tir)**2-2*b*np.cos(tir)**2*np.tan(tir)+(b**2+1)*np.cos(tir)**2))*math.radians(delta_theta_i) - (1/(b**2-2*tan_theta_i*b+tan_theta_i+1))*delta_b)
+
+
+print("theta_r= ", theta_r,"+-", delta_theta_r)
+
 
 n = np.sin(math.radians(theta_i))/np.sin(theta_r)
+delta_n = np.abs((np.cos(tir)/np.sin(theta_r))*math.radians(delta_theta_i) - ((np.sin(tir)*np.cos(theta_r))/np.sin(theta_r)**2)*delta_theta_r)
 
-print("n: ", n)
+print("n= ", n, "+-", delta_n)
 
 plt.errorbar(x, y, xerr=xerror, yerr=yerror, fmt='o', capsize=5, label="Dati con errore")
 
@@ -77,8 +94,10 @@ plt.ylabel(r'$\Delta x$ ($m$)')
 plt.title('Grafico con Bande di Errore')
 
 # Mostrare legenda
-plt.legend()
+#plt.legend()
 
 # Mostrare il grafico
-plt.savefig("grafico1.pdf", format="pdf")
-plt.show()
+#plt.savefig("grafico1.pdf", format="pdf")
+#plt.show()
+
+
